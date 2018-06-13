582351c0ce790c9b17b4c1fbb7bc5ee290950bdf
lior gishry <liorgish@gmail.com>
2018-06-13 22:37:57 +0300

    more

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -3,6 +3,10 @@ import {HeroDetailComponent} from "./hero-detail.component";
 import {ActivatedRoute} from "@angular/router";
 import {HeroService} from "../hero.service";
 import {Location} from '@angular/common'
+import {By} from "@angular/platform-browser";
+import {of} from "rxjs/observable/of";
+import {FormsModule} from "@angular/forms";
+import {Hero} from "../hero";
 
 describe('hero-detail.component', () => {
   let mockActivatedRoute, mockLocation, mockHeroService, fixture: ComponentFixture<HeroDetailComponent>
@@ -10,7 +14,7 @@ describe('hero-detail.component', () => {
 
     //mocks services
 
-    mockHeroService = jasmine.createSpyObj(['getero', 'updateHero'])
+    mockHeroService = jasmine.createSpyObj(['getHero', 'updateHero'])
     mockLocation = jasmine.createSpyObj(['back'])
     mockActivatedRoute = {
       snapshot: {
@@ -22,6 +26,7 @@ describe('hero-detail.component', () => {
       }
     }
     TestBed.configureTestingModule({
+      imports:[FormsModule],
       declarations: [HeroDetailComponent],
       providers: [
         {provide: ActivatedRoute, useValue: mockActivatedRoute},
@@ -30,10 +35,14 @@ describe('hero-detail.component', () => {
       ]
     })
     fixture = TestBed.createComponent(HeroDetailComponent);
+    mockHeroService.getHero.and.returnValue(of(<Hero>{name:'foo',id:1,strength:11}))
     fixture.detectChanges();
   })
 
   it('should render hero name inside h2 tag', () => {
-
+    // let h2NativeElement = fixture.debugElement.query(By.css('h2')).nativeElement;
+    fixture.detectChanges();
+    let h2NativeElement = fixture.nativeElement.querySelector('h2');
+    expect(h2NativeElement.textContent).toContain('FOO')
   })
 })
